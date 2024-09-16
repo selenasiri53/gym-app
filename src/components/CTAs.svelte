13fddbb12d9f6555b7a0ad1b3ca8c897e6899bae
Selena Siri <selenasiri53@gmail.com>
2024-09-16 12:31:19 -0400

    footer

--- src/components/CTAs.svelte
+++ src/components/CTAs.svelte
@@ -1,12 +1,13 @@
 <script>
-  
+  export let dark;
 </script>
 
 <div class="flex items-center gap-4">
-    <button class="specialBtn">
+    <button class={"specialBtn" + (dark ? " bg-[#181b34]"
+    : " ")}>
         <p class="text-base sm:text-lg md:text-xl">Contact Us</p>
     </button>
-    <button class="specialBtn">
+    <button class="specialBtnDark">
         <p class="text-base sm:text-lg md:text-xl">Get Started &rarr;</p>
     </button>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
   import Hero from "../components/Hero.svelte";
   import Product from "../components/Product.svelte";
   import Reviews from "../components/Reviews.svelte";
+  import FaQs from "../components/FAQs.svelte"
+  import Conversion from "../components/Conversion.svelte";
 </script>
 
 <main class="flex flex-col">
 	<Hero />
 	<Product />
 	<Reviews />
+  <FaQs />
+  <Conversion />
 </main>
